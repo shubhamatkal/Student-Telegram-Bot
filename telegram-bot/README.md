a987099e055ecad0375031c530ec1cca37be50fd
shubhamatkal <shubhamatkal333@gmail.com>
2023-11-12 00:39:26 +0530

    update readme

---
+++ telegram-bot/README.md
@@ -0,0 +1,71 @@
+# IITM-BS Telegram Bots for staying up to date with all the events and deadlines
+
+Welcome to the IIT Madras-BS Uodates Bot repository ! This repository contains two folders: one for a WhatsApp bot and another for a Telegram bot. These bots are designed to provide details about today's live sessions and schedules specifically tailored for students
+
+google docs for getting information about ongoing updates in this repo
+https://docs.google.com/document/d/1yjr_NXChKjF3HIbJfOVPO2BQ8AqG9RMoNGmtE4Ih1C0/edit?usp=sharing
+## Features
+
+- **WhatsApp Bot:**
+  - Retrieves today's live session details.
+  - Provides the schedule for the day.
+  - User-friendly interaction through WhatsApp.
+
+- **Telegram Bot:**
+  - Fetches information on live sessions happening today.
+  - Displays the daily schedule.
+  - Seamless interaction within the Telegram platform.
+
+## Getting Started for Contributing
+
+To use these bots, follow these steps:
+
+1. **Clone the Repository:**
+   ```bash
+   git clone https://github.com/your-username/iitm-bs-live-session-tracker.git
+   cd iitm-bs-live-session-tracker
+   ```
+
+3. **Install Dependencies:**
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+4. **Run the Bots:**
+   - Start the WhatsApp bot:
+     ```bash
+     cd WhatsApp\ bot
+     python main.py
+     ```
+   - Start the Telegram bot:
+     ```bash
+     cd Telegram\ bot
+     python main.py
+     ```
+
+## Configuration
+
+
+### Telegram Bot
+1. Open `Telegram bot/config.py`.
+2. Replace `"YOUR_TELEGRAM_BOT_TOKEN"` with your actual Telegram Bot Token.
+
+## Usage
+
+- **WhatsApp Bot:**
+  - Send a message to the Twilio WhatsApp Sandbox number with the keyword "sessions" or "schedule."
+
+- **Telegram Bot:**
+  - Start a chat with the Telegram bot and use the commands `/sessions` or `/schedule`.
+
+## Contributing
+
+We welcome contributions! If you have any suggestions, improvements, or bug fixes, feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+---
+
+Feel free to customize this README according to the specific details and intricacies of your project. Good luck with your live session tracker for IITM BS students!
--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -1,45 +1,38 @@
+#imports
 import telegram
-from telegram import Update
+from telegram import Update ,InlineKeyboardButton , InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters , Application , ContextTypes
 
 # Create a Telegram bot
-bot = telegram.Bot('6725664494:AAGxV47zV-ON7aPDOmHTxHqP8uSgQLDVZfo')
+token_main = None
+token_foundation = None
+token_diploma = None
+token_degree = None
+token_admin = None
 
-# Define a function to greet the user
-# def greet_user(update, context):
-#     bot.send_message(chat_id=update.message.chat_id, text='Hi there!')
-
-# # Define a function to guide the user through all the commands
-# def help_command(update, context):
-#     print("sending message user typed start")
-#     bot.send_message(chat_id=update.message.chat_id, text='Here are the commands you can use: /start:'
-#                                                           ' Start the bot /help: Get help with the '
-#                                                           'bot/greet: Greet the '
-#                                                           'bot/ask: Ask the bot a question')
-#
 async def ask_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("hello welcome")
+    await update.message.reply_text("hello welcome , type /selectcourse to select your course")
 
-# Define a function to respond to the user's selection
-# def respond_to_selection(update, context):
-#     # Get the user's selection
-#     selection = update.message.text
-#
-#     # Respond to the user's selection
-#     if selection == '/greet':
-#         bot.send_message(chat_id=update.message.chat_id, text='Hi there!')
-#     elif selection == '/ask':
-#         bot.send_message(chat_id=update.message.chat_id, text='What would you like to ask?')
-#     else:
-#         bot.send_message(chat_id=update.message.chat_id, text='Invalid selection.')
+async def ask_year(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends a message with three inline buttons attached."""
+    keyboard = [
+        [
+            InlineKeyboardButton("Foundation", callback_data="1"),
+            InlineKeyboardButton("Diploma", callback_data="2"),
+        ],
+        [InlineKeyboardButton("Degree", callback_data="3")],
+    ]
 
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("Please choose your course:", reply_markup=reply_markup)
 # Create an Application object
 # Create an Updater object
 application = Application.builder().token("6725664494:AAGxV47zV-ON7aPDOmHTxHqP8uSgQLDVZfo").build()
 
 application.add_handler(CommandHandler("start", ask_user))
 # application.add_handler(CommandHandler('help', help_command))
-# application.add_handler(CommandHandler('greet', greet_user))
+application.add_handler(CommandHandler('selectcourse', ask_year))
 #TODO work on these
 # application.add_handler(MessageHandler(filters.text, respond_to_selection))
 
