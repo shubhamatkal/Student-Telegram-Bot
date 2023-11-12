45b9665153467d4bed20a4568b76e4371e861cdb
shubhamatkal <shubhamatkal333@gmail.com>
2023-11-12 10:29:15 +0530

    update project

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -1,15 +1,16 @@
 #imports
-import telegram
-from telegram import Update ,InlineKeyboardButton , InlineKeyboardMarkup
+from telegram import *
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters , Application , ContextTypes
+import os
 
 # Create a Telegram bot
-token_main = None
-token_foundation = None
-token_diploma = None
-token_degree = None
-token_admin = None
+TOKEN_MAIN = os.environ.get("MAIN_BOT_TOKEN")
+TOKEN_FOUNDATION = os.environ.get("FOUNDATION_BOT_TOKEN")
+TOKEN_DIPLOMA = os.environ.get("DIPLOMA_BOT_TOKEN")
+TOKEN_DEGREE = os.environ.get("DEGREE_BOT_TOKEN")
+TOKEN_ADMIN = os.environ.get("ADMIN_BOT_TOKEN")
 
+#defining some funtions
 async def ask_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("hello welcome , type /selectcourse to select your course")
 
@@ -26,14 +27,21 @@ async def ask_year(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await update.message.reply_text("Please choose your course:", reply_markup=reply_markup)
-# Create an Application object
-# Create an Updater object
-application = Application.builder().token("6725664494:AAGxV47zV-ON7aPDOmHTxHqP8uSgQLDVZfo").build()
 
-application.add_handler(CommandHandler("start", ask_user))
+# Create an Application object
+main_bot = Application.builder().token(TOKEN_MAIN).build()
+foundation_bot = Application.builder().token(TOKEN_FOUNDATION).build()
+diploma_bot = Application.builder().token(TOKEN_DIPLOMA).build()
+degree_bot = Application.builder().token(TOKEN_DEGREE).build()
+admin_bot = Application.builder().token(TOKEN_ADMIN).build()
+
+#creating handlers
+main_bot.add_handler(CommandHandler("start", ask_user))
 # application.add_handler(CommandHandler('help', help_command))
-application.add_handler(CommandHandler('selectcourse', ask_year))
+main_bot.add_handler(CommandHandler('selectcourse', ask_year))
 #TODO work on these
 # application.add_handler(MessageHandler(filters.text, respond_to_selection))
 
-application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+#polling
+main_bot.run_polling(allowed_updates=Update.ALL_TYPES)
