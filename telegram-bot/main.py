bf9a36a568e354902274cc5be339ee0ca102340b
shubhajit07 <shubhajith85@gmail.com>
2023-11-17 21:35:56 +0530

    Added basic bot class

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -1,7 +1,6 @@
 #imports
 from flask import Flask, request , Response
-import requests
-import json
+from bot_functions import Bot
 
 '''
 if you dont have bot token generated use: 6965083380:AAHDWyCqvIhqYOAhi19gIF7u6CPcQKZ2qPM , 
@@ -9,37 +8,14 @@ bot link : https://t.me/test_main_iitmbs_bot
 but we suggest you to create your own bot token for development 
 '''
 BOT_TOKEN = ""
-base_url = f"https://api.telegram.org/bot{BOT_TOKEN}"
+bot = Bot(BOT_TOKEN)
 #insert your webhook url here, you can get it using vscode inbuilt tunneling in ports section
 # eg https://01tt20hz-8443.asse.devtunnels.ms/
 WEBHOOK_URL = ""
 
 app = Flask(__name__)
 
-#defining some bot functions
-def send_message(chat_id, text ):
-    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
-    payload = {"chat_id":chat_id, "text":text}
-    r  = requests.post(url, json=payload)
-    return r
-
-def delete_webhook():
-    request_url = base_url+'/setWebhook'
-    data = {
-        'url': ""
-    }
-    return requests.post(request_url, json=data).json()
-
-def set_webhook( webhook_url: str) -> dict:
-    request_url = base_url+'/setWebhook'
-    data = {
-        'url': webhook_url
-    }
-    return requests.post(request_url, json=data).json()
-
-delete_webhook()
-print("webhook deleted")
-set_webhook(WEBHOOK_URL)
+bot.set_webhook("https://t7mpzpbx-8443.inc1.devtunnels.ms/")
 
 @app.route("/" , methods= ['POST', 'GET'])
 def index():
@@ -47,11 +23,13 @@ def index():
         response_ = request.get_json()
         print("RESPONSE")
         print(response_)
-        if response_['message']['text'] == "hello":
-            send_message(chat_id=response_['message']['chat']['id'],text="this is test" )
+        chat = bot.validate_update(response_)
+        print(chat.update_type)
+        # if response_['message']['text'] == "hello":
+        #     bot.send_message(chat_id=response_['message']['chat']['id'],text="this is test" )
         return Response('ok', status= 200)
     else:
-      return "this is not post request"
+        return 'Bot is active now'
 
 
 #Notes:
@@ -59,4 +37,4 @@ def index():
 #https://api.telegram.org/bot6965083380:AAHDWyCqvIhqYOAhi19gIF7u6CPcQKZ2qPM/setWebhook?url=https://01tt20hz-8443.asse.devtunnels.ms/
 
 if __name__ == '__main__':
-    app.run(port=8443, debug=True, use_reloader=False)
+    app.run(port=8443, debug=True, use_reloader=True)
