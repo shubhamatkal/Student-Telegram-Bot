220a08bd439bcacdf67f7ae93a598d2b2060a24f
shubhajit07 <shubhajith85@gmail.com>
2023-11-21 00:17:54 +0530

    simple bug fixes

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -2,6 +2,7 @@
 from flask import Flask, request , Response
 import requests
 import json
+from bot_functions import Bot
 
 '''
 if you dont have bot token generated use: 6677627030:AAElX2DcR0vWqDGHdgbTio9og1DLJiMK5Mw , 
@@ -10,26 +11,24 @@ bot link : https://t.me/test_foundation_iitmbs_bot ,
 https://t.me/test_main_iitmbs_bot 
 but we suggest you to create your own bot token for development 
 '''
-BOT_TOKEN = ""
-bot = Bot(BOT_TOKEN)
+# BOT_TOKEN = "6965083380:AAHDWyCqvIhqYOAhi19gIF7u6CPcQKZ2qPM"
+bot = Bot('6286806337:AAFid6IhaNarajV91IqDc1APGvpy0PLt6No')
 #insert your webhook url here, you can get it using vscode inbuilt tunneling in ports section
 # eg https://01jt23tc-8443.asse.devtunnels.ms/
-WEBHOOK_URL = ""
+WEBHOOK_URL = "https://t7mpzpbx-8443.inc1.devtunnels.ms/"
 
 app = Flask(__name__)
 
-bot.set_webhook("https://t7mpzpbx-8443.inc1.devtunnels.ms/")
+bot.set_webhook(WEBHOOK_URL)
 
 @app.route("/" , methods= ['POST', 'GET'])
 def index():
     if request.method == 'POST':
         response_ = request.get_json()
-        print("RESPONSE")
-        print(response_)
         chat = bot.validate_update(response_)
-        print(chat.update_type)
-        # if response_['message']['text'] == "hello":
-        #     bot.send_message(chat_id=response_['message']['chat']['id'],text="this is test" )
+        if chat.message_type=='bot_command':
+            if chat.get_command() == 'start':
+                chat.send_message('Hello the bot has been started.\n<b>This bot is under development now</b>')
         return Response('ok', status= 200)
     else:
         return 'Bot is active now'
