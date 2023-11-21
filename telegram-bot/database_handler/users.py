4f277c93a842da65b98b48cecb90d6d6c9720588
shubhamatkal <shubhamatkal333@gmail.com>
2023-11-21 23:12:25 +0530

    create json for storage has error

--- telegram-bot/database_handler/users.py
+++ telegram-bot/database_handler/users.py
@@ -1,45 +1,26 @@
+import json
 import os
-import csv
 
+class UserDataManager:
+    def __init__(self, file_path):
+        self.file_path = file_path
 
-class User:
-    parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-    db_directory = os.path.join(parent_directory, "db")
-    def __init__(self):
-        # Create the 'db' directory if it doesn't exist
-        if not os.path.exists(self.db_directory):
-            os.makedirs(self.db_directory)
-        self.csv_file = os.path.join(self.db_directory, 'users.csv')
-        print(self.csv_file)
-        self.headers = ['chat id', 'username', 'registered Subjects', 'is admin', 'last login']
-        if not os.path.exists(self.csv_file):
-            self._create_csv()
+        if not os.path.exists(self.file_path):
+            # Create a default JSON structure if the file doesn't exist
+            self.data = {}
+            self._save_data()
+        else:
+            # Load existing JSON data
+            with open(self.file_path, 'r') as file:
+                self.data = json.load(file)
 
-    def _create_csv(self):
-        with open(self.csv_file, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(self.headers)
-
-    def is_chat_id_registered(self, chat_id):
-        with open(self.csv_file, 'r', newline='') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                if row['chat id'] == str(chat_id):
-                    return True
-            return False
+    def _save_data(self):
+        with open(self.file_path, 'w') as file:
+            json.dump(self.data, file, indent=4)
 
     def register_chat_id(self, chat_id):
-        with open(self.csv_file, 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow({chat_id: "chat id"})  # Empty placeholders for other columns
-        print(f"chat id : {chat_id} added to database")
-
-
-# #FOR TESTING ABOVE CLASS
-# if __name__ == '__main__':
-#     #Test the User class
-#     user = User()
-#     if user.is_chat_id_registered(1234567) == False:  # Check if chat ID 123456 is registered
-#         user.register_chat_id(1234567)  # Register chat ID 123456
-#     else:
-#         print("User already exists")
+        self.data['chat_id'] = chat_id
+        self._save_data()
+        
+    def is_chat_id_exist(self, chat_id):
+        return chat_id in self.data
--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -3,24 +3,24 @@ from flask import Flask, request, Response
 from bot_functions import Bot
 from messages import replies
 import os
+from database_handler import UserDataManager
 
-"""
-if you dont have bot token generated use: 6677627030:AAElX2DcR0vWqDGHdgbTio9og1DLJiMK5Mw , 
-6965083380:AAHDWyCqvIhqYOAhi19gIF7u6CPcQKZ2qPM , 
-bot link : https://t.me/test_foundation_iitmbs_bot , 
-https://t.me/test_main_iitmbs_bot 
-but we suggest you to create your own bot token for development 
-"""
 # Enter your telegram bot token here or set an environment variable named TG_BOT_TOKEN with the bot token
-# This will by default take the testing bot token
+# This will by default take the testing bot token(6677627030:AAElX2DcR0vWqDGHdgbTio9og1DLJiMK5Mw,
+# https://t.me/test_foundation_iitmbs_bot )
 BOT_TOKEN = os.environ.get(
-    "TG_BOT_TOKEN", "6965083380:AAHDWyCqvIhqYOAhi19gIF7u6CPcQKZ2qPM"
+    "TG_BOT_TOKEN", "6677627030:AAElX2DcR0vWqDGHdgbTio9og1DLJiMK5Mw"
 )
+
 bot = Bot(BOT_TOKEN)
 
 # insert your webhook url here, you can get it using vscode inbuilt tunneling in ports section
 # eg https://01jt23tc-8443.asse.devtunnels.ms/
-WEBHOOK_URL = ""
+WEBHOOK_URL = "https://01tt20hz-8443.asse.devtunnels.ms/"
+
+#initalize database
+file_path = '../db/users.json'
+db = UserDataManager(file_path)
 
 app = Flask(__name__)
 
@@ -35,7 +35,12 @@ def index():
         if chat.message_type == "bot_command":
             command = chat.get_command(argument=True)
             if command[0] == "start":
-                chat.send_message(replies.get("/start"))
+                #register_user
+                if db.is_chat_id_exist(response_["message"]["chat"]["id"]):
+                    chat.send_message(replies.get("/start_old_user"))
+                else:
+                    db.register_chat_id(response_["message"]["chat"]["id"])
+                    chat.send_message(replies.get("/start_new_user"))
             elif command[0] == "select":
                 keyboard = {
                     "inline_keyboard": [
