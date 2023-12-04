de9c1ab1c05f1aab699a87ca6a0f59e9bf44320b
shubhamatkal <shubhamatkal333@gmail.com>
2023-12-05 01:15:39 +0530

    inegrate mongo db

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -3,9 +3,9 @@ from flask import Flask, request, Response
 from bot_functions import Bot
 from messages import replies
 import os
-from database_handler import UserDataManager
+from database_handler import MongoDBHandler
 
-# Enter your telegram bot token here or set an environment variable named TG_BOT_TOKEN with the bot token
+# Enter your telegram bot token here
 # This will by default take the testing bot token(6677627030:AAElX2DcR0vWqDGHdgbTio9og1DLJiMK5Mw,
 # https://t.me/test_foundation_iitmbs_bot )
 BOT_TOKEN = os.environ.get(
@@ -14,13 +14,14 @@ BOT_TOKEN = os.environ.get(
 
 bot = Bot(BOT_TOKEN)
 
+#mongo db url(for testing and dev purpose create ur own db locally or using mongodb atlas and paste the link below)
+DB_URL = ""
+
+db = MongoDBHandler(DB_URL, "iitm-bot", "users")
+
 # insert your webhook url here, you can get it using vscode inbuilt tunneling in ports section
 # eg https://01jt23tc-8443.asse.devtunnels.ms/
 WEBHOOK_URL = ""
-
-#initalize database
-file_path = '../db/users.json'
-db = UserDataManager(file_path)
 
 app = Flask(__name__)
 
@@ -35,11 +36,14 @@ def index():
         if chat.message_type == "bot_command":
             command = chat.get_command(argument=True)
             if command[0] == "start":
+                print("user sent start command")
                 #register_user
-                if db.is_chat_id_exist(response_["message"]["chat"]["id"]):
+                if db.user_exists(response_["message"]["chat"]["id"]):
+                    print("user is old")
                     chat.send_message(replies.get("/start_old_user"))
                 else:
-                    db.register_chat_id(response_["message"]["chat"]["id"])
+                    print("user is new")
+                    db.add_chat_id(response_["message"]["chat"]["id"])
                     chat.send_message(replies.get("/start_new_user"))
             elif command[0] == "select":
                 keyboard = {
@@ -59,8 +63,13 @@ def index():
 
 
 # Notes:
-# https://api.telegram.org/bot6965083380:AAHDWyCqvIhqYOAhi19gIF7u6CPcQKZ2qPM/getMe
-# https://api.telegram.org/bot6965083380:AAHDWyCqvIhqYOAhi19gIF7u6CPcQKZ2qPM/setWebhook?url=https://01tt20hz-8443.asse.devtunnels.ms/
+# Add chat_ids
+# mongo_handler.add_chat_id("Bob")
+# # Add subjects to a specific user
+# mongo_handler.add_subjects_to_user("Shubham", ["Math", "Science"])
+# mongo_handler.add_subjects_to_user("Bob", ["History", "Geography"])
+
 
 if __name__ == "__main__":
     app.run(port=8443, debug=True, use_reloader=True)
+    db.close_connection()
